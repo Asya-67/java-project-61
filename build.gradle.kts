fc3f038a7ed049bd705931969de9a4e0121f1d03
Asya-67 <annadorovskikh111@gmail.com>
2025-02-05 22:40:44 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("java")
+    id("com.github.ben-manes.versions") version "0.42.0" // Проверьте на наличие самой свежей версии
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Здесь добавьте зависимости вашей библиотеки
+}
+
+
